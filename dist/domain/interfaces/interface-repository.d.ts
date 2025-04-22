b309909b115310f9e05345588e8e32d83e8f0414
Jonnyxjj <133527189+jhonnyxjj@users.noreply.github.com>
2025-04-22 17:55:22 -0300

    Delete dist directory

--- dist/domain/interfaces/interface-repository.d.ts
+++
@@ -1,7 +0,0 @@
-import { Task } from "../models/task.js";
-export interface InterfaceRepository {
-    listTasks(): Promise<Task[]>;
-    createTasks(task: Task): Promise<void>;
-    updateTasks(task: Task): Promise<Task>;
-    deleteTasks(id: number): Promise<void>;
-}
--- dist/domain/models/task.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Task {
-    id: number;
-    title: string;
-    description?: string;
-    completed?: boolean;
-    constructor(id: number, title: string, description?: string, completed?: boolean);
-}
